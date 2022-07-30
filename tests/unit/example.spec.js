cd15299369bbab823f2da433884aafdb964cc764
InnocentWahome <innocentwahome@gmail.com>
2022-07-30 15:38:57 +0300

    new test, navbar component

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,68 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+import Navbar from '@/components/Navbar.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueRouter)
+
+let wrapper
+let store
+let mockActions
+let router
+
+beforeEach(() => {
+  mockActions = {
+    setSelectedInstallerInitiativeId: jest.fn(),
+    setLoadingMode: jest.fn()
+  }
+
+  router = new VueRouter({
+    routes: []
+  })
+
+  store = new Vuex.Store({
+    state: {
+      system: {
+        isNavBarVisible: true,
+        isAsideMobileExpanded: false,
+      },
+      authentication: {
+        firstName: "wahome",
+        lastName: "wahome",
+        userEmail: "wahome@example.com",
+      },
+      isNavBarVisible: true,
+    },
+    actions: mockActions,
+    getters: {
+      getInstallerInitiatives: () => []
+    }
+  })
+
+  wrapper = mount(Navbar, {
+    store,
+    localVue,
+    router,
+    stubs: [
+      'base-button',
+      'router-link',
+      'initiative-title-image',
+      'base-spinner',
+      'font-awesome-icon'
+    ]
+  })
+
+  wrapper.setData({
+    active_filter: 'all'
+  })
+})
+
+describe('Installer Initiative', () => {
+  it('has a navbar-brand class', () => {
+    const initiatives = wrapper.find('.navbar-brand')
+    expect(initiatives.exists()).toBe(true)
+  })
+})
